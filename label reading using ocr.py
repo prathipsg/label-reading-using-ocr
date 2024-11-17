c8f7e69f78618e9cfc513fa7772cb091440d2c58
Pratheeka P <110621614+prathipsg@users.noreply.github.com>
2024-11-17 16:50:06 +0530

    Add files via upload

---
+++ label reading using ocr.py
@@ -0,0 +1,15 @@
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+def recText(filename):
+    text=pytesseract.image_to_string(Image.open(filename))
+    return text
+info=recText('TEST1.webp')
+print(info)
+file=open("New.txt","w")
+file.write(info)
+file.close()
+print("Written Successful")
